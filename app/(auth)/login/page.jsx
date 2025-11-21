1f5d335fb9c55c3fd55d46a4f9204d52b31e3673
Bipin <bipinsyadav9@gmail.com>
2025-11-21 18:26:30 +0530

    setup done

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Store data in localStorage as object
+    const userData = { email, password };
+    localStorage.setItem("userData", JSON.stringify(userData));
+
+    redirect("/dashboard");
+    // Optional: Clear form
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            width: "300px",
+            margin: "50px auto",
+            textAlign: "center",
+
+            border: "1px solid #ccc",
+            padding: "20px",
+            borderRadius: "8px",
+          }}
+        >
+          <h2>Login Form</h2>
+          <form onSubmit={handleSubmit}>
+            {/* Email Field */}
+            <div style={{ marginBottom: "10px" }}>
+              <p>Email</p>
+              <input
+                type="email"
+                value={email}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter email"
+                style={{ width: "100%", padding: "8px" }}
+              />
+            </div>
+
+            {/* Password Field */}
+            <div style={{ marginBottom: "10px" }}>
+              <p>Password</p>
+
+              <input
+                type="password"
+                value={password}
+                required
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter password"
+                style={{ width: "100%", padding: "8px" }}
+              />
+            </div>
+
+            <button
+              type="submit"
+              style={{
+                width: "100%",
+                padding: "10px",
+                background: "black",
+                color: "white",
+                cursor: "pointer",
+              }}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ app/(protected)/dashboard/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Cards from "@/app/components/Cards/page";
+import { redirect } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Dashboard = () => {
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    // Check if user data exists in localStorage
+    if (typeof window !== "undefined") {
+      let useData = localStorage.getItem("userData");
+      if (!useData) {
+        redirect("/login");
+      } else {
+        setUserData(useData);
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome, {userData ? JSON.parse(userData).email : "Guest"}!</p>
+      <Cards />
+    </div>
+  );
+};
+
+export default Dashboard;
